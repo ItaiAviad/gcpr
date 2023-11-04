3e1ba55b9be39d3e72626ee0df0a98d853cad178
Itai Aviad <aviaditai22@gmail.com>
2023-11-04 09:47:24 +0200

    Fixed minor problems (exit in raise_msg)

--- gcpr.py
+++ gcpr.py
@@ -88,14 +88,15 @@ def raise_msg(msg: str, code: int, flags: list = [], color: bool = True, exit: b
             print(f'{bcolors.BOLD}{bcolors.FAIL}{msg if msg else MESSAGE_ERROR}{bcolors.ENDC}{bcolors.ENDC}')
         else:
             print(f'{msg if msg else MESSAGE_ERROR}')
-        sys.exit(CODE_ERROR)
+        
     elif ((code == CODE_OK and FLAG_QUIET not in flags) or force):
         if color:
             print(f'{bcolors.BOLD}{bcolors.OKGREEN}{msg if msg else MESSAGE_OK}{bcolors.ENDC}{bcolors.ENDC}')
         else:
             print(f'{msg if msg else MESSAGE_OK}')            
-        if exit:
-            sys.exit(CODE_OK)
+
+    if exit:
+        sys.exit(CODE_OK) if code == CODE_OK else sys.exit(CODE_ERROR)
 
 def get_files_data(argv: list, flags: list) -> (list, str):
     # Get all the given files
@@ -194,7 +195,7 @@ def main_compilation(files: list, extension: str, flags: list):
             raise_msg("Error: Executable File not found.", CODE_ERROR, flags)
         run_command = [f'./{executable_filename}']
         run_res = subprocess.run(run_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
-        print(run_res.stdout + run_res.stderr, end='')
+        raise_msg(f'{run_res.stdout + run_res.stderr}', CODE_OK, flags, color=False, force=True)
 
         if (run_res.returncode == CODE_OK):
             raise_msg("Done!", CODE_OK, flags)
